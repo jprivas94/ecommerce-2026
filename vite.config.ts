d77a9d5ec5722606de02773b5ec15fa168d2ebe9
Jean Rivas <jprivas.ro@gmail>
2026-01-05 00:11:56 -0500

    FIX AUTH

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
-})
+	plugins: [react()],
+	server: {
+		proxy: {
+			'/api': {
+				target: 'https://ecommerce-backend-j2zb.onrender.com',
+				changeOrigin: true,
+				secure: false,
+			},
+		},
+	},
+});
